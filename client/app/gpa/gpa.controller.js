d1c41d49a0250a1f5b38e8895660958d9625bd5e
bayer106 <bayer106@morris.umn.edu>
2015-02-16 15:36:59 -0600

    finally got stuff from the database (added a single s)

--- client/app/gpa/gpa.controller.js
+++ client/app/gpa/gpa.controller.js
@@ -4,9 +4,13 @@ angular.module('jsonDataProcessingLabApp')
   .controller('GpaCtrl', function ($scope, $http, socket) {
     $scope.myStudents = [];
 
-    $http.get('/api/student').success(function(myStudents) {
+    $http.get('/api/students').success(function(myStudents) {
       $scope.myStudents = myStudents;
       socket.syncUpdates('student', $scope.myStudents);
     });
+
+    $scope.returnName = function(){
+      return $scope.myStudents[0].firstName;
+    };
   });
 
