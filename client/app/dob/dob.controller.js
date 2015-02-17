2d78612e91356874c086941ddadd81635973c289
Ben Simondet <simon998@morris.umn.edu>
2015-02-17 12:56:01 -0600

    Added sort to date of birth. Added beginnings of major sorting.

--- client/app/dob/dob.controller.js
+++ client/app/dob/dob.controller.js
@@ -9,10 +9,21 @@ angular.module('jsonDataProcessingLabApp')
       socket.syncUpdates('student', $scope.myStudents);
     });
 
-    $scope.returnName = function(){
+    $scope.returnName = function() {
       return $scope.myStudents[0].firstName;
-
-
     };
-  });
+
+    $scope.orderBy = function (property) {
+        var sortOrder = 1;
+        if(property[0] === "-") {
+          sortOrder = -1;
+          property = property.substr(1);
+        }
+        return function (a,b) {
+          var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+          return result * sortOrder;
+        }
+      }
+    });
+
 
--- client/app/major/major.controller.js
+++ client/app/major/major.controller.js
@@ -12,5 +12,17 @@ angular.module('jsonDataProcessingLabApp')
     $scope.returnName = function(){
       return $scope.myStudents[0].firstName;
     };
+
+    $scope.orderBy = function (property) {
+      var sortOrder = 1;
+      if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+      }
+      return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+      }
+    }
   });
 
