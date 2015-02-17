b18749a78f2acdb75e15243dd540db6bc3538a07
bayer106 <bayer106@morris.umn.edu>
2015-02-17 13:52:12 -0600

    had merge problem

--- client/app/major/major.controller.js
+++ client/app/major/major.controller.js
@@ -20,7 +20,17 @@ angular.module('jsonDataProcessingLabApp')
         property = property.substr(1);
       }
       return function (a,b) {
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        //var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0; one line doesn't handle null cases
+        var isPropertyBNull = b[property] !== null;
+        var isPropertyANull = a[property] !== null;
+        var result = 0;
+        if ((a[property] < b[property]) && isPropertyBNull) {
+          result = -1;
+        } else if ((a[property] > b[property]) &&  isPropertyANull) {
+          result=1;
+        } else {
+          result = 2;
+        }
         return result * sortOrder;
       }
     }
