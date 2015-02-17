7b4664e1378b21f1db929592c52fe4f23d6270dd
Ben Simondet <simon998@morris.umn.edu>
2015-02-17 13:48:33 -0600

    Added most functionality for search by student status. Need to display students.

--- client/app/studentStatus/studentStatus.controller.js
+++ client/app/studentStatus/studentStatus.controller.js
@@ -3,11 +3,61 @@
 angular.module('jsonDataProcessingLabApp')
   .controller('StudentStatusCtrl', function ($scope, $http, socket) {
     $scope.myStudents = [];
+    $scope.statusSort = [];
+    $scope.getStatusSort = function(){
+      $scope.sortByStatus();
+      return $scope.statusSort;
+    };
+    $scope.classSelected = 0;
 
     $http.get('/api/students').success(function(myStudents) {
       $scope.myStudents = myStudents;
       socket.syncUpdates('student', $scope.myStudents);
     });
+
+    $scope.sortByStatus = function(){
+      for (var i = 0; i < $scope.myStudents.length; i++) {
+        var totalCredits = 0;
+        for(var j=0; j< $scope.myStudents.courses.length; j++){
+          totalCredits+= parseInt($scope.myStudents[i].courses[j].credits);
+          console.log("got here");
+        }
+        if($scope.classSelected = 1){
+          console.log("gothere");
+          if(totalCredits < 30){
+            $scope.statusSort.push($scope.myStudents[i]);
+          }
+        }else if($scope.classSelected = 2){
+          if(totalCredits > 30 && totalCredits < 60){
+            $scope.statusSort.push($scope.myStudents[i]);
+          }
+        }else if($scope.classSelected = 3){
+          if(totalCredits > 60 && totalCredits < 90){
+            $scope.statusSort.push($scope.myStudents[i]);
+          }
+        }else if($scope.classSelected = 4){
+          if(totalCredits > 90 && totalCredits < 120){
+            $scope.statusSort.push($scope.myStudents[i]);
+          }
+        }else if($scope.classSelected = 5){
+          if(totalCredits > 120) {
+            $scope.statusSort.push($scope.myStudents[i]);
+          }
+        }
+      }
+    };
+
+    $scope.orderBy = function (property) {
+      var sortOrder = 1;
+      if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+      }
+      return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+      }
+    };
 
     $scope.returnName = function(){
       return $scope.myStudents[0].firstName;
