78dca285c2c99aaeb517c292a0de6d0157763d03
Ben Simondet <simon998@morris.umn.edu>
2015-02-21 13:40:05 -0600

    Search for students by course complete.

--- client/app/course/course.controller.js
+++ client/app/course/course.controller.js
@@ -1,66 +1,27 @@
 'use strict';
 
 angular.module('jsonDataProcessingLabApp')
-  .controller('AllCoursesCtrl', function ($scope, $http, socket) {
+  .controller('CourseCtrl', function ($scope, $http, socket) {
     $scope.myStudents = [];
-    $scope.searchedCourse = [];
-    $scope.searchedCourseStudents = [];
-    $scope.searchResultsArray = [];
     $scope.searchField = "";
-    $scope.toReturn = "";
-    $scope.courseToSearchFor = "";
+    $scope.classResults = [];
 
-    $http.get('/api/students').success(function (myStudents) {
+    $http.get('/api/students').success(function(myStudents) {
       $scope.myStudents = myStudents;
       socket.syncUpdates('student', $scope.myStudents);
     });
 
-    $scope.searchResults = function(){
-      var str;
-      var n;
-      for(var i = 0; i<$scope.myStudents.length; i++){
-        for(var j = 0; j<$scope.myStudents[i].courses.length; j++){
-          str = $scope.myStudents[i].courses[j].course.name;
-          n = str.search(/$scope.searchField/i);
-          if(n != -1){
-            $scope.searchResultsArray.push(str);
+    $scope.searchForStudent = function(){
+      $scope.classResults = [];
+      for (var i = 0; i < $scope.myStudents.length; i++) {
+        for (var j = 0; j < $scope.myStudents[i].courses.length; j++) {
+          if ($scope.myStudents[i].courses[j].course.name == $scope.searchField) {
+            $scope.classResults.push($scope.myStudents[i]);
           }
         }
       }
-      if($scope.searchResultsArray.length == 1){
-        $scope.toReturn = $scope.searchResultsArray[0];
-      } else if($scope.searchResultsArray.length < 1){
-        alert("No classes were found. Try checking your spelling.");
+      if($scope.classResults.length == 0){
+        alert("No Courses with that name were found. Please check spelling and remember this search is case-sensitive.");
       }
     };
-
-    $scope.searchForCourse = function(){
-
-      for (var i = 0; i < $scope.myStudents.length; i++) {
-        if ($scope.myStudents[i].firstName == $scope.fixedSearchField()){
-          $scope.searchedCourse.push($scope.myStudents[i]);
-        } else if ($scope.myStudents[i].lastName == $scope.fixedSearchField()){
-          $scope.searchedCourse.push($scope.myStudents[i]);
-        }
-      }
-      if ($scope.searchedCourse.length > 1){
-        alert("We found more than one course with that name.");
-      }else if (!$scope.searchedCourse.length > 0) {
-        alert("We couldn't find any course by that name. Try checking your spelling.");
-      } else {
-        $scope.toReturn = $scope.searchedCourse[0].firstName + " " + $scope.searchedStudent[0].lastName;
-        $scope.loopThroughCourses();
-      }
-      $scope.searchField = "";
-    };
-
-    $scope.returnCourseSearched = function(){
-      return $scope.toReturn;
-    };
-
-    $scope.loopThroughCourses = function(){
-      for (var i = 0; i<$scope.searchedStudent[0].courses.length; i++){
-        $scope.searchedStudentClasses.push($scope.searchedStudent[0].courses[i]);
-      }
-    }
   });
